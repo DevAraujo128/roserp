2f47e3c6b463b54e8c83ec889def3958b225ce11
João Rodrigues <joaorodriguesjp01@gmail.com>
2026-09-18 23:42:20 -0300

    🎉 Commit inicial

---
+++ src/app.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+	return (
+		<main className="min-h-svh w-full text-zinc-100 bg-zinc-900 grid place-content-center gap-4 p-4">
+			<h1>RoseRP</h1>
+		</main>
+	);
+}
